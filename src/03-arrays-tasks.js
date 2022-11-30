bab9836307ef66a0f9e52d86d35de0b44b0b9438
Ihar Skavysh <igogoblin@gmail.com>
2022-11-30 21:50:14 +0300

    feat: start third task

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -20,8 +20,8 @@
  *    ['Array', 'Number', 'string'], 'Date'    => -1
  *    [0, 1, 2, 3, 4, 5], 5    => 5
  */
-function findElement(/* arr, value */) {
-  throw new Error('Not implemented');
+function findElement(arr, value) {
+  return arr.indexOf(value);
 }
 
 /**
@@ -35,8 +35,13 @@ function findElement(/* arr, value */) {
  *    2 => [ 1, 3 ]
  *    5 => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  const result = [];
+  // eslint-disable-next-line no-plusplus
+  for (let i = 1, j = 1; j <= len; i += 2, j++) {
+    result.push(i);
+  }
+  return result;
 }
 
 
